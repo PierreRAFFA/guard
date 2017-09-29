fa074f1021268db6f5c6a3f1f0ae10c9f570b3f8
PierreRAFFA <pierre.raffa@gmail.com>
2017-09-29 14:30:29 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# guard
+# Node ACL
+
+This library provides a minimalistic ACL implementation with no connection to a database.
+
+### Installation
+```bash
+$ npm install node-acl --save
+
+
+```
+
+### Define the authorisations
+It's recommended to deny access to all, then allows the routes for a specific role.
+
+```js
+acl.add(['any'], 'any' , '.*', 'deny');
+acl.add(['admin', 'developer'], 'any' , '.*', 'allow');
+acl.add(['authenticated', 'marketing'], 'GET' , '/api/cases/.*', 'allow');
+acl.add(['authenticated', 'marketing'], 'POST' , '/api/cases/.*', 'allow');
+acl.add(['marketing'], 'DELETE' , '/api/cases/.*', 'deny');
+```
+
+### Check the access
+Checks the access by getting the user roles
+```js
+const user = {
+    ...
+    roles: ['authenticated'],
+  };
+acl.can(user, 'GET', '/api/cases/123-abc/comments/456-abc') // return true
+```
+```js
+const user = {
+    roles: ['marketing']
+  };
+acl.can(user, 'GET', '/api/cases/123-abc') // return true
+acl.can(user, 'POST', '/api/cases/123-abc') // return true
+acl.can(user, 'DELETE', '/api/cases/123-abc') // return false
+```
+
+### Custom user structure
+```js
+
+acl.setRolesGetter(user => {
+    const roles = [];
+    if (user.admin) {
+      roles.push('admin');
+    }
+    if (user.developer) {
+      roles.push('developer');
+    }
+    return roles;
+});
+
+const user = {
+    ...
+    admin: true,
+    authenticated: true,
+  };
+
+acl.can(user, 'GET', '/api/cases') // return true
+```
+
+### Documentation
+
+#### add(roles, verb, url, permission)
+Adds a specific access control.  
+
+>*roles* {Array<string>} Any role that you want. any may be used a wildcard  
+*verb* {string} GET|POST|PATCH|PUT|DELETE|any (wildcard)  
+*url* {string} RegExp route  
+*permission* {string} allow|deny  
+  
+#### can(user, verb, url)
+Returns if the user have access to a specific route.  
+
+>*roles* {Array<string>} Any role that you want. any may be used a wildcard  
+*verb* {string} GET|POST|PATCH|PUT|DELETE|any (wildcard)  
+*url* {string} RegExp route  
+
+#### setRolesGetter(method)  
+Defines a custom method to retrieve the user roles  
+
+>*method* {Function} Function called when acl needs to retrieve the user roles  
+
---
+++ test/acl.test.js
@@ -0,0 +1,192 @@
+/**
+ * Created by pierreraffa on 28/09/2017.
+ */
+const test = require('ava').test;
+const acl = require('../lib');
+const reduce = require('lodash/reduce');
+
+test.beforeEach(t => {
+  acl.clear();
+});
+
+test('NO access specified so any user should have access', async t => {
+  const user = {
+    roles: ['authenticated']
+  };
+  t.is(acl.can(user, 'GET', '/api/cases'), true);
+});
+
+
+test('User with a role matching an denied access should NOT have access', async t => {
+  acl.add(['authenticated'], 'GET' , '/api/cases', 'deny');
+
+  const user = {
+    roles: ['authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), false)
+});
+
+
+test('When a deny access for all, no user should have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+
+  const user = {
+    roles: ['anonymous']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), false)
+});
+
+test('User with an array of role matching an allowed access should have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['authenticated'], 'GET' , '/api/cases', 'allow');
+
+  const user = {
+    roles: ['admin', 'authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), true)
+});
+
+test('User with an array of role NOT matching an allowed access should NOT have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['developer'], 'GET' , '/api/cases', 'allow');
+
+  const user = {
+    roles: ['admin', 'authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), false)
+});
+
+test('When a authenticated access allows some POST url with wildcard, any authenticated should have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['authenticated'], 'POST' , '/api/cases/.*', 'allow');
+
+  const user = {
+    roles: ['authenticated']
+  };
+
+  t.is(acl.can(user, 'POST', '/api/cases/123-abc'), true)
+});
+
+test('When a authenticated access denies some POST url with wildcard, any authenticated should NOT have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['authenticated'], 'POST' , '/api/cases/.^[\/]*', 'allow');
+
+  const user = {
+    roles: ['authenticated']
+  };
+
+  t.is(acl.can(user, 'POST', '/api/cases/123-abc/rr'), false)
+});
+
+test('When a authenticated access allows some url with wildcard, any authenticated should have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['authenticated'], 'GET' , '/api/cases/.*/comments/.*', 'allow');
+
+  const user = {
+    roles: ['authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases/123-abc/comments/456-abc'), true)
+});
+
+test('When a authenticated access allows some url with wildcard, any authenticated should NOT have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['authenticated'], 'GET' , '/api/cases/.*/comments/.*', 'deny');
+
+  const user = {
+    roles: ['authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases/123-abc/comments/456-abc'), false)
+});
+
+test('When a admin access allows all, any admin should have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['admin'], 'GET' , '.*', 'allow');
+
+  const user = {
+    roles: ['admin', 'authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), true)
+});
+
+test('When a admin access allows all, any admin with other role should have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['admin'], 'GET' , '/api/.*', 'allow');
+
+  const user = {
+    roles: ['admin', 'authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), true)
+});
+
+test('When a admin access allows all, any user other than admin should NOT have access', async t => {
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['admin'], 'GET' , '.*', 'allow');
+
+  const user = {
+    roles: ['authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), false)
+});
+
+test('When a custom roles getter is specified, the user is admin, any admin should have access', async t => {
+  acl.setRolesGetter(user => {
+    return user.customRoles;
+  });
+
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['admin'], 'GET' , '/api/.*', 'allow');
+
+  const user = {
+    customRoles: ['admin', 'authenticated']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), true)
+});
+
+test('When a custom roles getter is specified, the user is admin, any admin should NOT have access', async t => {
+  acl.setRolesGetter(user => {
+    return user.customRoles;
+  });
+
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['authenticated'], 'GET' , '/api/.*', 'allow');
+
+  const user = {
+    customRoles: ['anonymous']
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), false)
+});
+
+
+test('When a custom roles getter is specified, the user is admin, any admin should have access', async t => {
+  acl.setRolesGetter(user => {
+    const roles = [];
+    if (user.admin) {
+      roles.push('admin');
+    }
+    if (user.developer) {
+      roles.push('developer');
+    }
+    return roles;
+  });
+
+  acl.add(['any'], 'any' , '.*', 'deny');
+  acl.add(['admin'], 'GET' , '/api/.*', 'allow');
+
+  const user = {
+    admin: true,
+    developer: true,
+  };
+
+  t.is(acl.can(user, 'GET', '/api/cases'), true)
+});
